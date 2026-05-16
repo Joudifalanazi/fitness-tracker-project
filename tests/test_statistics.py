c6df259437e0847dbbf18b5794a8dbdac558221c
zX <168583455+Humanxzx@users.noreply.github.com>
2026-05-16 20:06:44 +0300

    Update test_statistics.py

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,11 +1,25 @@
 import unittest
-from statistics import calculate_performance_index
+
+from statistics import (
+    calculate_pace_distribution,
+    calculate_performance_index
+)
+
 
 class TestStatistics(unittest.TestCase):
 
+    def test_pace_distribution(self):
+
+        result = calculate_pace_distribution(30, 5)
+
+        self.assertEqual(result, 6.0)
+
     def test_performance_index(self):
-        result = calculate_performance_index(10, 500)
-        self.assertEqual(result, 50)
+
+        result = calculate_performance_index(300, 5)
+
+        self.assertEqual(result, 350.0)
+
 
 if __name__ == "__main__":
     unittest.main()
