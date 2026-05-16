55cdd40203c188788c97b27b344a671032bca6d8
lamismohammed919-commits <lamismohammed919@gmail.com>
2026-05-16 19:21:14 +0300

    Create test_workout.py

---
+++ tests/test_workout.py
@@ -0,0 +1,21 @@
+import unittest
+from workout import create_workout
+
+class TestWorkout(unittest.TestCase):
+
+    def test_create_workout(self):
+
+        workout = create_workout(
+            "2026-05-16",
+            "Running",
+            30,
+            5,
+            300,
+            145
+        )
+
+        self.assertEqual(workout["exercise_type"], "Running")
+        self.assertEqual(workout["duration_minutes"], 30)
+
+if __name__ == "__main__":
+    unittest.main()
